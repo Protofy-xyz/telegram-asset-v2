0ba3ca96b167bc1c85ce120e0afce94d3c946663
miquel <miquel@protofy.xyz>
2025-09-19 11:58:58 +0200

    Adds support for nesting multiple level on board states
    Co-authored-by: lluis@protofy.xyz <lluis@protofy.xyz>

--- packages/protonode/src/lib/boardConnect.ts
+++ packages/protonode/src/lib/boardConnect.ts
@@ -3,6 +3,86 @@
 const protobase = require('protobase')
 const protonode = require('protonode')
 const API = protobase.API
+function toPath(path) {
+    return path
+        .replace(/\?\./g, ".")           // remove optional chaining
+        .replace(/\[(["']?)(.*?)\1\]/g, ".$2") // convert [x] or ["x"] into .x
+        .split(".")
+        .filter(Boolean);
+}
+
+function getByPath(obj, path, def?) {
+    return toPath(path).reduce((acc, key) => acc?.[key], obj) ?? def;
+}
+
+const castValueToType = (value, type, boardStates) => {
+    switch (type) {
+        case 'string':
+            return String(value);
+        case 'number':
+            return Number(value);
+        case 'boolean':
+            return value === 'true' || value === true;
+        case 'json':
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return {};
+            }
+        case 'array':
+            try {
+                return JSON.parse(value);
+            } catch (e) {
+                return [];
+            }
+        case 'card':
+            return value; // Assuming card is a string identifier
+        case 'text':
+            return value; // Assuming markdown is a string
+        case "state":
+            return JSON.stringify(getByPath(boardStates, value))
+        default:
+            return value; // Default case, return as is
+    }
+}
+
+const isState = (value) => {
+    if (typeof value !== 'string') return false;
+    // check if value starts with 'board.' or board?. or board[ or board?.[
+    return value.startsWith('board.') || value.startsWith('board?.') || value.startsWith('board[') || value.startsWith('board?.[');
+}
+
+const getStateName = (value) => {
+    const statePath = toPath(value)
+    return statePath[1]
+}
+
+export const resolveBoardParam = async ({ states, boardId, defaultValue, value, type }) => {
+    let resolved = false;
+
+    if (value === undefined || value === null || value === '') {
+        value = defaultValue
+    }
+
+    // create boardStates due state value starts with board.
+    const boardsStates = { board: states?.boards?.[boardId] ?? {} };
+
+    if (typeof value === 'string' && isState(value)) {
+        const stateName = getStateName(value);
+        if (states?.boards?.[boardId] && states.boards[boardId][stateName] !== undefined) {
+            value = getByPath(boardsStates, value)
+            resolved = true;
+        } else {
+            console.warn('State ' + stateName + ' not found in board ' + boardId);
+        }
+    }
+
+    if (type && !resolved) {
+        value = castValueToType(value, type, boardsStates);
+    }
+
+    return value;
+}
 
 export function boardConnect(run) {
     let context = {} as {
@@ -15,8 +95,8 @@ export function boardConnect(run) {
     const token = protonode.getServiceToken()
     const listeners = {}
 
-    const onChange = ({ name, key = undefined,  changed, inmediate = false }) => {
-        if(!name && key) {
+    const onChange = ({ name, key = undefined, changed, inmediate = false }) => {
+        if (!name && key) {
             console.warn('onChange called with key but no name, using key as name. key is a deprected parameter, please use name instead.');
             name = key;
         }
@@ -24,12 +104,12 @@ export function boardConnect(run) {
             listeners[name] = [];
         }
         listeners[name].push(changed);
-        if(inmediate) {
+        if (inmediate) {
             changed(context.states[name]);
         }
     }
 
-    async function execute_action({ name, params = {}, done = (result) => {}, error = (err) =>{} }) {
+    async function execute_action({ name, params = {}, done = (result) => { }, error = (err) => { } }) {
         console.log('Executing action boardConnect: ', name, params);
         const action = context.actions[name]
         if (!action) {
@@ -49,22 +129,14 @@ export function boardConnect(run) {
         //if the param is not visible, hardcode the param value to the value in the configParams defaultValue
         if (action.configParams) {
             for (const param in action.configParams) {
-                // if(action.configParams[param].visible === false && action.configParams[param].defaultValue !== '') {
-                //     params[param] = action.configParams[param].defaultValue
-                // }
-                if(action.configParams[param].defaultValue !== '') {
-                    // si el param empieza por board. lo sustituimos por el valor del context.boardId
-                    // compruba que el defaultValue es un string
-                    if(typeof action.configParams[param].defaultValue === 'string' && action.configParams[param].defaultValue.startsWith('board.')) {
-                        const stateName = action.configParams[param].defaultValue.substring(6);
-                        if(context.states[stateName] && context.states[stateName] !== undefined) {
-                            params[param] = context.states[stateName];
-                        } else {
-                            console.warn('State ' + stateName + ' not found in board ' + context.boardId);
-                        }
-                    } else {
-                        params[param] = action.configParams[param].defaultValue;
-                    }
+                if (action.configParams && action.configParams[param]) {
+                    params[param] = await resolveBoardParam({
+                        states: context.states,
+                        boardId: context.boardId,
+                        defaultValue: action.configParams[param].defaultValue,
+                        value: params[param],
+                        type: action.configParams[param]?.type
+                    });
                 }
             }
         }
@@ -76,7 +148,7 @@ export function boardConnect(run) {
             }
             //console.log('url: ', url+'?token='+token)
             const response = await API.post(url + '?token=' + (token ? token : protonode.getServiceToken()), data);
-            if(response.isError) {
+            if (response.isError) {
                 console.error('Error executing action: ', response.error);
                 error(response.error);
                 return;
@@ -87,7 +159,7 @@ export function boardConnect(run) {
             const paramsStr = Object.keys(params).map(k => k + '=' + params[k]).join('&');
             //console.log('url: ', url+'?token='+token+'&'+paramsStr)
             const response = await API.get(url + '?token=' + token + '&' + paramsStr);
-            if(response.isError) {
+            if (response.isError) {
                 console.error('Error executing action: ', response.error);
                 error(response.error);
                 return;
