f4a91e92eaf6a323ac1f9f5116052eec96e98c61
noreplydev <contact.cristiansanchez@gmail.com>
2025-10-02 18:09:37 +0200

    Add board store hooks
    Co-authored-by: miquel <miquel@protofy.xyz>

--- extensions/boards/pages/view.tsx
+++ extensions/boards/pages/view.tsx
@@ -475,6 +475,10 @@ const Board = ({ board, icons }) => {
 
   //@ts-ignore store the states in the window object to be used in the cards htmls
   window['protoStates'] = states
+
+  //@ts-ignore 
+  window['protoBoardName'] = board.name
+
   //@ts-ignore emit an event to notify the states have changed
   window.dispatchEvent(new CustomEvent('protoStates:update', { detail: { board: board.name } }))
 
