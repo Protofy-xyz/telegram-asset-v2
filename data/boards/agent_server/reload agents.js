2eec4d48dbf1cca8a88d10e236d00c80b1faeb8a
jcarlosn <jcarlos.norte@gmail.com>
2025-10-22 17:13:27 +0200

    Adds name to the agent props returned by the agent server

--- data/boards/agent_server/reload agents.js
+++ data/boards/agent_server/reload agents.js
@@ -2,16 +2,16 @@ const aliases = await context.state.getStateTree({ chunk: 'proxy' });
 const boards = aliases.boards || {};
 
 const filtered = Object.entries(boards)
-  .filter(([_, value]) => 
-    Object.values(value).some(v => 
-      typeof v.alias === 'string' && /^\/api\/agents\/v1\/.+/.test(v.alias)
+  .filter(([_, value]) =>
+    Object.values(value).some(
+      v => typeof v.alias === "string" && /^\/api\/agents\/v1\/.+/.test(v.alias)
     )
   )
   .map(([key, value]) => {
-    const alias = Object.values(value).find(v => 
-      typeof v.alias === 'string' && /^\/api\/agents\/v1\/.+/.test(v.alias)
+    const alias = Object.values(value).find(
+      v => typeof v.alias === "string" && /^\/api\/agents\/v1\/.+/.test(v.alias)
     )?.alias;
-    return [key, { target: alias }];
+    return [key, { name: key, target: alias }];
   });
 
 const filteredObj = Object.fromEntries(filtered);
