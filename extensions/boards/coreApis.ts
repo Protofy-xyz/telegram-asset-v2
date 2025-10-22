da0f39ee8226f3f37e34a1e604a85ff2f933c0a8
miquel <miquel@protofy.xyz>
2025-10-22 16:44:05 +0200

    Makes system boards visible from boards page

--- extensions/boards/coreApis.ts
+++ extensions/boards/coreApis.ts
@@ -241,12 +241,13 @@ const getDB = (path, req, session, context?) => {
                     const decodedContent = JSON.parse(fileContent);
 
                     const userType = session?.user?.type;
+                    const isAllVisible = req.query.all === 'true';
                     const isSystem = !!decodedContent?.tags?.includes('system');
                     const usersList = Array.isArray(decodedContent?.users) ? decodedContent.users : null;
 
                     let allowed: boolean;
 
-                    if (isSystem) {
+                    if (isSystem && !isAllVisible) {
                         // System boards: show ONLY if a whitelist exists and includes this user type
                         allowed = !!(usersList && userType && usersList.includes(userType));
                     } else {
