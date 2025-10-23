494dd297c461fefe22bd83e8b6fe45e4c6e8ca70
jcarlosn <jcarlos.norte@gmail.com>
2025-10-23 04:50:48 +0200

    Modify ai agent tandem to not use board automation to handle agentic request, freeing the automation for the normal board use

--- data/templates/boards/ai agent tandem/ai agent tandem.js
+++ data/templates/boards/ai agent tandem/ai agent tandem.js
@@ -2,21 +2,7 @@ const { boardConnect } = require('protonode')
 const { Protofy } = require('protobase')
 
 const run = Protofy("code", async ({ context, states, board }) => {
-board.onChange({
-    name: 'user_request',
-    changed: async (value) => {
-        if (value) {
-            await board.execute_action({
-                name: 'agent_prepare',
-                params: { prompt: value }
-            });
-            await board.execute_action({
-                name: 'agent_core',
-                params: {}
-            });
-        }
-    }
-});
+
 })
 
 boardConnect(run)
